16fff2861a8bac8b19b4b5eeb4ab7a35072c0442
David Oliveira <90005502@pmenos.com.br>
2025-04-19 13:19:39 -0300

    Including migration for new Rent field.

---
+++ MotoLocadora.Infrastructure/Migrations/Application/20250419153534_AlteracaoNovoCampoRent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotoLocadora.Infrastructure.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AlteracaoNovoCampoRent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "EstimatedPrice",
+                table: "Rents",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedPrice",
+                table: "Rents");
+        }
+    }
+}
